6a8c003741b7a8809e19c25fe9b1d941ff22b047
Amjad Alissa <amjad-alissa@outlook.com>
2023-02-06 17:13:25 +0100

    update Index.java handel the generated randomShuffel

--- src/main/java/de/adesso/alissa/yahtzee/controller/Index.java
+++ src/main/java/de/adesso/alissa/yahtzee/controller/Index.java
@@ -1,23 +1,26 @@
 package de.adesso.alissa.yahtzee.controller;
 
 import de.adesso.alissa.yahtzee.elements.Table;
-import de.adesso.alissa.yahtzee.elements.TableRow;
+import de.adesso.alissa.yahtzee.game.RandomShuffel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Index {
 
+    Table table = new Table();
+    RandomShuffel randomShuffel = new RandomShuffel();
+
     @GetMapping("/")
-    public String getIndexView(Model model, @ModelAttribute ArrayList<TableRow> upperSection,
-                               @ModelAttribute ArrayList<TableRow> lowerSection) {
-        Table table = new Table();
+    public String getIndexView(Model model) {
+        randomShuffel.generateRadomShuffel();
         model.addAttribute("upperSection", table.getUpperSection());
         model.addAttribute("lowerSection", table.getLowerSection());
+        model.addAttribute("radomDices", randomShuffel.getRandomDices());
+
         return "index";
     }
 
@@ -29,5 +32,18 @@ public class Index {
     @GetMapping("/more")
     public String getMoreView() {
         return "more";
+    }
+
+    @GetMapping("/randomShuffel")
+    public String getRandomShuffel(Model model) {
+        return getIndexView(model);
+    }
+
+
+
+    @PostMapping("/randomShuffel")
+    public String getRandomShuffel(Model model, @RequestParam boolean[] selectedDices) {
+        randomShuffel.updateSelectedDices(selectedDices);
+        return getIndexView(model);
     }
 }
