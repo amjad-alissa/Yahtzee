56abb82710e2fa5045a762d621cc3438bc07b510
Amjad Alissa <amjad-alissa@outlook.com>
2023-02-06 12:44:40 +0100

    Update score.html Create the table with th:each

--- src/main/java/de/adesso/alissa/yahtzee/controller/Index.java
+++ src/main/java/de/adesso/alissa/yahtzee/controller/Index.java
@@ -1,13 +1,23 @@
 package de.adesso.alissa.yahtzee.controller;
 
+import de.adesso.alissa.yahtzee.elements.Table;
+import de.adesso.alissa.yahtzee.elements.TableRow;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
 
 @Controller
 public class Index {
 
     @GetMapping("/")
-    public String getIndexView() {
+    public String getIndexView(Model model, @ModelAttribute ArrayList<TableRow> upperSection,
+                               @ModelAttribute ArrayList<TableRow> lowerSection) {
+        Table table = new Table();
+        model.addAttribute("upperSection", table.getUpperSection());
+        model.addAttribute("lowerSection", table.getLowerSection());
         return "index";
     }
 
