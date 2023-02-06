a6f88b0ad6a23ca1cfee24244ff3664914bdd22f
Amjad Alissa <amjad-alissa@outlook.com>
2023-02-06 09:33:02 +0100

    initial commit
    Initialize Project

---
+++ src/main/java/de/adesso/alissa/yahtzee/YahtzeeApplication.java
@@ -0,0 +1,13 @@
+package de.adesso.alissa.yahtzee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahtzeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YahtzeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/adesso/alissa/yahtzee/YahtzeeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adesso.alissa.yahtzee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahtzeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
